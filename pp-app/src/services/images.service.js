063b496d095278a6725f48d760955982a6e1b69f
MaleBurs <maleburs@gmail.com>
2022-11-10 17:49:49 -0300

    Update on routes

--- pp-app/src/services/images.service.js
+++ pp-app/src/services/images.service.js
@@ -4,16 +4,16 @@ import AuthService from "./auth.service";
 const upload = async(file) => {
     const fd = new FormData()
     fd.append('file', file)
-    await axios.post(`http://localhost:8080/profile/picture/${AuthService.getCurrentUser().id}`, fd)
+    await axios.post(`https://pp-app-backend.herokuapp.com/api/profile/picture/${AuthService.getCurrentUser().id}`, fd)
 }
 
 const getImageUrl = async() => {
-    const { data } = await axios.get(`http://localhost:8080/profile/picture/${AuthService.getCurrentUser().id}`, { responseType: 'blob' })
+    const { data } = await axios.get(`https://pp-app-backend.herokuapp.com/api/profile/picture/${AuthService.getCurrentUser().id}`, { responseType: 'blob' })
     const blob = new Blob([data], { type: 'image' })
     return URL.createObjectURL(blob)
 }
 const getMilestoneUrl = async(milestoneId) => {
-    const { data } = await axios.get(`http://localhost:8080/milestone/picture/${milestoneId}`, { responseType: 'blob' })
+    const { data } = await axios.get(`https://pp-app-backend.herokuapp.com/api/milestone/picture/${milestoneId}`, { responseType: 'blob' })
     const blob = new Blob([data], { type: 'image' })
     return URL.createObjectURL(blob)
 }
