910fd4f19bf0609ba1a9e07b0cdc497e264bc908
MaleBurs <maleburs@gmail.com>
2022-11-11 10:51:04 -0300

    Update

--- pp-app/src/Context/CurrentUserContext.js
+++ pp-app/src/Context/CurrentUserContext.js
@@ -17,7 +17,7 @@ export function CurrentUserContextProvider (props) {
   useEffect(() => {
     ImageService.getImageUrl().then((url) => {
       setProfilePictureURL(url);
-    }).catch()
+    }).catch(console.log("hola"))
   }, []);
 
   const value = useMemo(() => {
