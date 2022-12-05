5d047b4d09cd0abb6f69b9f67091ac86273f11b5
paulaaletti <82065887+paulaaletti@users.noreply.github.com>
2022-12-04 22:40:36 -0300

    Update StartDonation.jsx

--- pp-app/src/Components/Forms/Donations/MakeDonation/StartDonation.jsx
+++ pp-app/src/Components/Forms/Donations/MakeDonation/StartDonation.jsx
@@ -8,12 +8,12 @@ import ModalWithDetails from "../../../Utiles/ModalWithDetails";
 import { useNavigate } from "react-router-dom"
 import Messages from '../../Messages'
 import ActServices from '../../../../services/activities.service'
-import AdminServices from '../../../../services/transactions.service'
 
 const StartDonation = () => {
   const { selectedFrequency } = useFrequency();
   const {selectedAmount} = useAmount();
   const { paymentDay, subsPeriod} = useSubscriptionPeriod();
+  const [preferenceId, setPreferenceId] = useState(null);
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
   const currentUser = AuthService.getCurrentUser();
@@ -26,6 +26,38 @@ const StartDonation = () => {
   useEffect(() => {
     setMessage("");
   }, [selectedFrequency])
+
+  useEffect(() => {
+    console.log("entro");
+    if (preferenceId) {
+      console.log("entro al if");
+      // con el preferenceId en mano, inyectamos el script de mercadoPago
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://sdk.mercadopago.com/js/v2';
+      document.body.appendChild(script);
+      script.addEventListener('load', () => {
+        const mp = new window.MercadoPago('APP_USR-2ea7a2db-fb60-4ec4-a7e6-7dc2de112d06', {
+          locale: 'es-AR'
+        });
+        const checkout = mp.checkout({
+          preference: {
+            id: preferenceId
+          },
+          render: {
+            container: '.cho-container',
+            label: 'Pagar con Mercado Pago',
+          },
+          theme: {
+            elementsColor: '#0F6938',
+            headerColor: '#e7e6e6'
+          },
+          openMode: 'modal',
+          autoOpen: true,
+        })
+      });
+    }
+  }, [preferenceId]);
 
 
   const closeModalSuccess = () => {
@@ -54,7 +86,7 @@ const StartDonation = () => {
       DonationService.generateSubscription(currentUser.id, selectedAmount, selectedFrequency, subsPeriod.value, paymentDay).then(
         () => {
           setShowModalSuccess(true);
-          ActServices.createActivity(newSubscriptionEvenctDescription(selectedAmount, subsPeriod.label, paymentDay).title, newSubscriptionEvenctDescription(selectedAmount, subsPeriod.label, paymentDay).description, currentUser.id). then(
+          ActServices.createActivity(newSubscriptionEvenctDescription(selectedAmount, subsPeriod.label, paymentDay).title, newSubscriptionEvenctDescription(selectedAmount, subsPeriod.label, paymentDay).description, currentUser.id).then(
             (res)=> console.log(res)
           )
           //window.location.reload();
@@ -72,8 +104,10 @@ const StartDonation = () => {
         DonationService.createAndPayTransaction(currentUser.id, selectedAmount,"onlyTime", currentUser.name, currentUser.lastname).then(
           (response) => {
             console.log(response);
-            if(response.data.url){
-              window.location.href = response.data.url
+            if(response.data){
+              // window.location.href = response.data.url
+              setPreferenceId(response.data.preferenceId)
+              console.log("preferencia seteada");
             }
             //navigate("a donde querramos mandar");
             //window.location.reload();
@@ -94,7 +128,6 @@ const StartDonation = () => {
 
   return (
     <>
-    
     {showModalSuccess ? (
       <ModalWithDetails value={showModalSuccess} onChange={closeModalSuccess} header={(selectedFrequency === 1) ? "Tu donación ha sido realizada con éxito!" : "Tu suscripción ha sido activada con éxito!"} action={(selectedFrequency === 1) ? "Se realizó una donación de " : "Se activó una suscripción de "} body={"Muchas gracias por realizar una donación para brindar atención nutricional a niños/as de la comunidad."} buttonText={"Continuar"}></ModalWithDetails>
     ) : null}
@@ -102,6 +135,8 @@ const StartDonation = () => {
         className="rounded-xl p-3 h-auto w-full text-center greenBg yellowBgHover font-Pop-SB text-base text-white">
         {(selectedFrequency === 1)  ? "Donar" : "Donar periódicamente"}
       </button>
+      <div className='cho-container flex justify-center'>
+    </div>
        {message && (
         <Messages.ErrorMessage message={message}/>
         )}
