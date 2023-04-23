95f870c2d41a99e0ba349517c74adeec570925b9
MaleBurs <maleburs@gmail.com>
2023-04-23 10:57:46 -0300

    Starting Mi Perfil

--- pp-app/src/Components/Profile/CopyLinkSection.js
+++ pp-app/src/Components/Profile/CopyLinkSection.js
@@ -42,7 +42,7 @@ const CopyLinkSection = () => {
 
   return (
     <>
-    <div className="border bg-white rounded-md -mt-7 p-6 space-y-4">
+    <div className="border bg-white rounded-md -mt-5 p-6 space-y-4">
     <div className="font-Pop-M text-sm text-center purpleText"> ¡Haz crecer tu impacto de vida compartiendo tu enlace! </div>
     <div className="font-Pop-L text-xs text-center text-gray-400"> Al compartir tu enlace con tus amigos nos ayudas a llegar a miles de personas más y a expandir nuestra comunidad </div>
     
--- pp-app/src/Components/Profile/UserInformationSection.js
+++ pp-app/src/Components/Profile/UserInformationSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useCurrentUser } from "../../Context/CurrentUserContext";
 import LogoCorazon from "../../Components/Images/LogoCorazon.png";
 import logoHerramienta from "../../Components/Images/logoHerramienta.png";
+import NiñaSaltandoFondoAmarillo from "../../Components/Images/NinaSaltandoFondoAmarillo.png";
 import { useNavigate } from "react-router-dom";
 
 const UserInformationSection = (props) => {
@@ -12,7 +13,7 @@ const UserInformationSection = (props) => {
     <>
     <div className = "flex flex-col space-y-4 md:space-y-0 md:flex-row z-10 basis-1/3 border border-[#e7e6e6] mt-6 md:mt-14 divide-x divide-[#e7e6e6]">
        
-        <div className="py-6 pl-6 md:pl-12 lg:pl-32 pr-12 basis-1/2 flex flex-row space-x-5 md:space-x-6 lg:space-x-10 items-center">
+        <div className="py-6 pl-6 md:pl-12 lg:pl-24 pr-12 basis-1/2 flex flex-row space-x-5 md:space-x-6 lg:space-x-10 items-center">
             {profilePicture!=="null" ? 
             <img
                 className="object-cover h-24 w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 rounded "
@@ -29,7 +30,7 @@ const UserInformationSection = (props) => {
             </div>
         </div> 
     
-        <div className = "bg-[#F6F7F3] pt-6 pb-10 pr-6 md:pr-12 lg:pr-32 basis-1/2 md:justify-end items-start flex flex-row space-x-10">
+        <div className = "bg-[#F6F7F3] pt-6 pb-10 pr-6 md:pr-12 lg:pr-24 basis-1/2 md:justify-end items-start flex flex-row space-x-10">
             <div className="flex flex-col justify-center items-center z-10 py-1">
                 <button onClick={()=>navigate("/historialDonaciones")} className={`rounded-full w-fit ${props.select==="impacto" ? "bg-[#fdfcfc] border-[#e7e6e6] border" : "hover:bg-[#fdfcfc] hover:border-[#e7e6e6] hover:border"}`}>
                     <img
@@ -43,6 +44,21 @@ const UserInformationSection = (props) => {
                     <div className="font-Pop-L text-[10.5px] text-center w-32" >Ver mis donaciones y mi impacto de vida.</div>
                 </div>  
             </div>
+
+            <div className="flex flex-col justify-center items-center z-10 py-1">
+                <button onClick={()=>navigate("/miPerfil")} className={`rounded-full w-fit ${props.select==="miPerfil" ? "bg-[#fdfcfc] border-[#e7e6e6] border" : "hover:bg-[#fdfcfc] hover:border-[#e7e6e6] hover:border"}`}>
+                    <img
+                        className="object-cover h-10 w-10 md:h-20 md:w-20 "
+                        src={NiñaSaltandoFondoAmarillo}
+                        alt="ProfilePhoto"
+                    />
+                </button>
+                <div className="flex flex-col space-y-1">
+                    <div className="font-Pop-R text-xs text-center underline">Mi Perfil</div> 
+                    <div className="font-Pop-L text-[10.5px] text-center w-32" >Ver y configurar mi perfil público, para compartirlo con amigos.</div>
+                </div>  
+            </div>    
+
             <div className="flex flex-col justify-center items-center z-10 py-1">
                 <button onClick={()=>navigate("/settings")} className={`rounded-full w-fit ${props.select==="ajustes" ? "bg-[#fdfcfc] border-[#e7e6e6] border " : "hover:bg-[#fdfcfc] hover:border-[#e7e6e6] hover:border"}`}>
                     <img
